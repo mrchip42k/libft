4bb8132f3f121ba2dcc1242d80c0329b6774c2d5
Alessandro Metzen <ametzen@k0r3p5.42mulhouse.fr>
2022-11-09 15:11:59 +0100

    strtrim OK

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,12 +6,22 @@
 /*   By: ametzen <ametzen@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 09:56:47 by ametzen           #+#    #+#             */
-/*   Updated: 2022/11/09 14:36:41 by ametzen          ###   ########.fr       */
+/*   Updated: 2022/11/09 15:11:48 by ametzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Necessary for malloc()
 #include <stdlib.h>
+
+static size_t	st_strlen(char const *str)
+{
+	size_t	len;
+
+	len = 0;
+	while (str[len])
+		len++;
+	return (len);
+}
 
 // return 1 if (c) is one of (set), else return 0.
 static int	st_is_trimmable(char c, char const *set)
@@ -26,16 +36,6 @@ static int	st_is_trimmable(char c, char const *set)
 		set_i++;
 	}
 	return (0);
-}
-
-static size_t	st_strlen(char const *str)
-{
-	size_t	len;
-
-	len = 0;
-	while (str[len])
-		len++;
-	return (len);
 }
 
 // Index of first character that SHOULD NOT be trimmed,
@@ -54,32 +54,20 @@ static size_t	st_find_start(char const *s1, char const *set)
 	return (char_i);
 }
 
-// Index of the first TRIMMABLE character to the right of the end block
+// Index of the first TRIMMABLE character to the right of the end block.
+// Last 0 return is equivalent to char_i.
 static size_t	st_find_end(char const *s1, char const *set)
 {
 	size_t	char_i;
-	size_t	set_i;
-	int		b_found_trimmable;
 
 	char_i = st_strlen(s1);
 	while (char_i != 0)
 	{
 		char_i--;
-		b_found_trimmable = 0;
-		set_i = 0;
-		while (set[set_i])
-		{
-			if (s1[char_i] == set[set_i])
-			{
-				b_found_trimmable = 1;
-				break ;
-			}
-			set_i++;
-		}
-		if (!b_found_trimmable)
+		if (!st_is_trimmable(s1[char_i], set))
 			return (char_i + 1);
 	}
-	return (char_i);
+	return (0);
 }
 
 char	*ft_strtrim(char const *s1, char const *set)
@@ -87,13 +75,21 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*new_string;
 	size_t	start_idx;
 	size_t	end_idx;
+	size_t	i;
 
 	start_idx = st_find_start(s1, set);
 	end_idx = st_find_end(s1, set);
-	new_string = malloc(end_idx - start_idx + 1);
+	if (start_idx >= end_idx)
+		start_idx = 0;
+	new_string = malloc((end_idx - start_idx) + 1);
 	if (new_string)
 	{
-		
+		i = 0;
+		while ((i + start_idx) < end_idx)
+		{
+			new_string[i] = s1[start_idx + i];
+			i++;
+		}
 		new_string[end_idx - start_idx] = '\0';
 	}
 	return (new_string);
