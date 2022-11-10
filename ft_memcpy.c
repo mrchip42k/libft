14d21f08751f28c52fa3daf6636f477cc5c38551
Alessandro Metzen <ametzen@k0r3p5.42mulhouse.fr>
2022-11-10 10:02:13 +0100

    Add memcpy comment

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,19 @@
 /*   By: ametzen <ametzen@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/09 18:25:44 by ametzen           #+#    #+#             */
-/*   Updated: 2022/11/09 18:49:21 by ametzen          ###   ########.fr       */
+/*   Updated: 2022/11/10 10:01:19 by ametzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 // Necessary for size_t
 #include <stddef.h>
 
+// https://github.com/alelievr/libft-unit-test wants me to add the first
+//  if special case.
+// I don't understand why this is a good behaviour, or why the function
+//  can't also skip if either of them are null (|| instead of &&).
+// Unless this behaviour comes from the original memcpy, and had to be carried
+//  on for compatibility.
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
 	size_t	i;
