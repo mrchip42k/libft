50049edf27db864f9d54bd681aaa3fe97cb19b5f
Alessandro Metzen <ametzen@k0r3p5.42mulhouse.fr>
2022-11-10 13:49:48 +0100

    Rename variabe

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: ametzen <ametzen@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/10 13:33:04 by ametzen           #+#    #+#             */
-/*   Updated: 2022/11/10 13:40:06 by ametzen          ###   ########.fr       */
+/*   Updated: 2022/11/10 13:43:04 by ametzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*ch_s;
-	unsigned char		ch_c;
+	const unsigned char	*converted_s;
+	unsigned char		converted_c;
 	size_t				i;
 
-	ch_s = s;
-	ch_c = c;
+	converted_s = s;
+	converted_c = c;
 	i = 0;
 	while (i < n)
 	{
-		if (ch_s[i] == ch_c)
-			return ((void *)&ch_s[i]);
+		if (converted_s[i] == converted_c)
+			return ((void *)&converted_s[i]);
 		i++;
 	}
 	return (0);
