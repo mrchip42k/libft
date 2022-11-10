db0f2202927e64bcb0709e00dfa23478b290c1c7
Alessandro Metzen <ametzen@k0r3p5.42mulhouse.fr>
2022-11-10 17:43:30 +0100

    bonus lstadd_back OK

---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ametzen <ametzen@student.42mulhouse.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/10 17:39:36 by ametzen           #+#    #+#             */
+/*   Updated: 2022/11/10 17:43:12 by ametzen          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*current;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		current = *lst;
+		while (current->next)
+			current = current->next;
+		current->next = new;
+	}
+}
