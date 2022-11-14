619dbf8a2e98e6f216c5a345a32f57c1fe9205a9
Alessandro Metzen <ametzen@k0r3p5.42mulhouse.fr>
2022-11-14 13:54:11 +0100

    The Great Headerening

--- ft_prot_memset.c
+++ ft_prot_memset.c
@@ -6,7 +6,7 @@
 /*   By: ametzen <ametzen@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 12:06:34 by ametzen           #+#    #+#             */
-/*   Updated: 2022/11/14 12:57:16 by ametzen          ###   ########.fr       */
+/*   Updated: 2022/11/14 13:42:55 by ametzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,6 @@
 void	*ft_prot_memset(void *b, int c, size_t len)
 {
 	if (b == NULL)
-		return (b);
+		return (NULL);
 	return (ft_memset(b, c, len));
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: ametzen <ametzen@student.42mulhouse.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 10:46:56 by ametzen           #+#    #+#             */
-/*   Updated: 2022/11/14 13:16:36 by ametzen          ###   ########.fr       */
+/*   Updated: 2022/11/14 13:53:45 by ametzen          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,128 +27,178 @@
 // - General explanation on how to use the function, or what it does, goes here-
 
 ////////////////////////////////////////////////////////////////////////////////
-////  Part 1 : Replicas of libc functions  /////////////////////////////////////
+///////////////////////////////////////////////////////////////////   ----  ////
+////  Part 1 : Replicas of libc functions  ///////////////////////  /      /////
+/////////////////////////////////////////////////////////////////   ----  //////
 ////////////////////////////////////////////////////////////////////////////////
 
 // 💥 TLDR: these assume their arguments are correct, else crash.
 
 // 📂 libc replica
 int			ft_isalpha(int c);
+
 // 📂 libc replica
 int			ft_isdigit(int c);
+
 // 📂 libc replica
 int			ft_isalnum(int c);
+
 // 📂 libc replica
 int			ft_isascii(int c);
+
 // 📂 libc replica
 int			ft_isprint(int c);
+
 // 📂 libc replica
-// 💥 (s) == NULL
+// 💥 (s) == NULL (🛡 Protected version available)
 size_t		ft_strlen(const char *s);
+
 // 📂 libc replica
-// 💥 (b) == NULL
+// 💥 (b) == NULL (🛡 Protected version available)
 void		*ft_memset(void *b, int c, size_t len);
+
 // 📂 libc replica
-// 💥 (s) == NULL
+// 💥 (s) == NULL (🛡 Protected version available)
 void		ft_bzero(void *s, size_t n);
+
 // 📂 libc replica
 // 💥 either (src) || (dst) == NULL,
 //    DOESNT crash if BOTH (src) && (dst) == NULL
+// (🛡 Protected version available)
 void		*ft_memcpy(void *dst, const void *src, size_t n);
+
 // 📂 libc replica
-// 💥 Same crash rules as ft_memcpy, since it calls it.
+// 💥 Same crash rules as ft_memcpy, since it calls it. (🛡 Protected available)
 void		*ft_memmove(void *dst, const void *src, size_t len);
+
 // 📂 libc replica
-// 💥 (dst || src) == NULL
+// 💥 (dst || src) == NULL (🛡 Protected version available)
 size_t		ft_strlcpy(char *dst, const char *src, size_t dstsize);
+
 // 📂 libc replica
-// 💥 (src) == NULL
+// 💥 (src) == NULL (🛡 Protected version available)
 size_t		ft_strlcat(char *dst, const char *src, size_t dstsize);
+
 // 📂 libc replica
 int			ft_toupper(int c);
+
 // 📂 libc replica
 int			ft_tolower(int c);
+
 // 📂 libc replica
-// 💥 (s) == NULL
+// 💥 (s) == NULL (🛡 Protected version available)
 char		*ft_strchr(const char *s, int c);
+
 // 📂 libc replica
-// 💥 (s) == NULL
+// 💥 (s) == NULL (🛡 Protected version available)
 char		*ft_strrchr(const char *s, int c);
+
 // 📂 libc replica
 // 💥 (s1 || s2) == NULL
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
+
 // 📂 libc replica
-// 💥 (s) == NULL
+// 💥 (s) == NULL (🛡 Protected version available)
 void		*ft_memchr(const void *s, int c, size_t n);
+
 // 📂 libc replica
 // 💥 (s1 || s2) == NULL
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
+
 // 📂 libc replica
-// 💥 (haystack || needle) == NULL
+// 💥 (haystack || needle) == NULL (🛡 Protected version available)
 // (len) is the limit of (haystack), not (needle)
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
+
 // 📂 libc replica
-// 💥 (str) == NULL
+// 💥 (str) == NULL (🛡 Protected version available)
 int			ft_atoi(const char *str);
+
 // ❗️ Uses: malloc()
 // 📂 libc replica
 // ❓ No protection for 0 size or count? TODO figure this out
 void		*ft_calloc(size_t count, size_t size);
+
 // ❗️ Uses: malloc()
 // 📂 libc replica
-// 💥 (s1) == NULL
+// 💥 (s1) == NULL (🛡 Protected version available)
 char		*ft_strdup(const char *s1);
 
 ////////////////////////////////////////////////////////////////////////////////
-////  Part 2 : Other  //////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////  /----  --+--  ////
+////  Part 2 : Other  /////////////////////////////////////  /----    /    /////
+//////////////////////////////////////////////////////////  /        /    //////
 ////////////////////////////////////////////////////////////////////////////////
 
 // ℹ️ See libft subject for original documentation
 
 // ❗️ Uses: malloc()
+// 📂 Part 2
 // Allocate a new string,
 // copied from (s), starting from (start), up to (len) long.
 char		*ft_substr(char const *s, unsigned int start, size_t len);
+
 // ❗️ Uses: malloc()
+// 📂 Part 2
 // Allocate a new string,
 // concatenating (s1) and (s2).
 char		*ft_strjoin(char const *s1, char const *s2);
+
 // ❗️ Uses: malloc()
+// 📂 Part 2
 // Allocate a new string, long at most like (s1),
 // that trims occurrences of characters in (set) at the start and end of (s1).
 char		*ft_strtrim(char const *s1, char const *set);
+
 // ❗️ Uses: malloc(), free()
+// 📂 Part 2
 // Allocate a null terminated array of pointers
 //   to allocated new strings,
 // splitting (s) with the character (c).
 // Array will be null if allocating either the array or any string fails.
 char		**ft_split(char const *s, char c);
+
 // ❗️ Uses: malloc()
+// 📂 Part 2
 // Allocate a new string,
 // representing the signed integer (n) as a string.
 char		*ft_itoa(int n);
+
 // ❗️ Uses: malloc()
+// 📂 Part 2
 // Allocate a new string, with the same length as (s),
 // where each character is the return of (f()).
 // (f()) takes the index of the current character, and the character from (s).
 char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+// 📂 Part 2
 // Apply the modifying function (f()) to each character of the string (s).
 // (f()) takes the index of the current character,
 // and the pointer to the current character from (s).
 void		ft_striteri(char *s, void (*f)(unsigned int, char*));
+
 // ❗️ Uses: write()
+// 📂 Part 2
 void		ft_putchar_fd(char c, int fd);
+
 // ❗️ Uses: write()
+// 📂 Part 2
 void		ft_putstr_fd(char *s, int fd);
+
 // ❗️ Uses: write()
+// 📂 Part 2
 // Like ft_putstr_fd, but appends a newline at the end.
 void		ft_putendl_fd(char *s, int fd);
+
 // ❗️ Uses: write()
+// 📂 Part 2
 // Accepts anything in the signed int type.
 void		ft_putnbr_fd(int n, int fd);
 
 ////////////////////////////////////////////////////////////////////////////////
-////  Bonus : Linked lists  ////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////       * -> *      ////
+////  Bonus : Linked lists  ////////////////////////////       /'     \.   /////
+///////////////////////////////////////////////////////  * -> *        *  //////
 ////////////////////////////////////////////////////////////////////////////////
 
 typedef struct s_list
@@ -157,25 +207,42 @@ typedef struct s_list
 	struct s_list	*next;
 }					t_list;
 
+// 📂 Bonus
 // ❗️ Uses: malloc()
 t_list		*ft_lstnew(void *content);
+
+// 📂 Bonus
 void		ft_lstadd_front(t_list **lst, t_list *new);
+
+// 📂 Bonus
 int			ft_lstsize(t_list *lst);
+
+// 📂 Bonus
 t_list		*ft_lstlast(t_list *lst);
+
+// 📂 Bonus
 void		ft_lstadd_back(t_list **lst, t_list *new);
+
 // ❗️ Uses: free()
-// 💥 ( del() ) == NULL.
+// 📂 Bonus
+// 💥 ( del() ) == NULL. (🛡 Protected version available)
 // I'd rather find out this crashed, than let this silently leak memory
 void		ft_lstdelone(t_list *lst, void (*del)(void*));
+
 // ❗️ Uses: free()
+// 📂 Bonus
 // 💥 ( lst || del() ) == NULL. (🛡 Protected version available)
 // I'd rather find out this crashed, than let this silently leak memory
 void		ft_lstclear(t_list **lst, void (*del)(void*));
+
+// 📂 Bonus
 // Content is NOT null checked. That's the responsibility of ( f() ).
 // ( f() ) will be called for EVERY item in the list.
 // ( f() ) will be called as many times as there are list items.
 void		ft_lstiter(t_list *lst, void (*f)(void *));
+
 // ❗️ Uses: malloc(), free()
+// 📂 Bonus
 // ( f() ) needs to malloc the element it returns. (f() may return NULL)
 // Does nothing and returns NULL if (lst), ( f() ), or ( del() ) are NULL.
 // (More info in source code)
@@ -183,19 +250,9 @@ void		ft_lstiter(t_list *lst, void (*f)(void *));
 t_list		*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
 ////////////////////////////////////////////////////////////////////////////////
-////  Personal additions ft++ : Moved from static  /////////////////////////////
-////////////////////////////////////////////////////////////////////////////////
-
-// 📂 ft++ (libc replica)
-int			ft_isspace(char c);
-// 📂 ft++
-// 💥 (dst || src) == NULL
-// Similar to ft_memcpy.
-// Goes from right to left, skips the special case check and does not return.
-void		ft_mini_memcpy_left(void *dst, const void *src, size_t len);
-
-////////////////////////////////////////////////////////////////////////////////
-////  Personal additions ft++ : Protected versions  ////////////////////////////
+////////////////////////////////////////////////////////////////  ++++++++  ////
+////  Personal additions ft++ : Protected versions  ///////////  ++++++++  /////
+//////////////////////////////////////////////////////////////  ++++++++  //////
 ////////////////////////////////////////////////////////////////////////////////
 
 // ℹ️ A comment on protected functions
@@ -207,67 +264,93 @@ void		ft_mini_memcpy_left(void *dst, const void *src, size_t len);
 // Prot fn only care about "I should never crash. Not my problem". Nothing else.
 // Use braincells, as usual. Read the comments. Read the implementations.
 
-// 📂 ft++ / prot libc
+// 📂 ft++ / 🛡 prot libc
 // Like strlen, but returns 0 instead of crashing if (s) == NULL.
 size_t		ft_prot_strlen(const char *s);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like memset, but does nothing if (b) == NULL.
+// 🤔 Probably useless
 void		*ft_prot_memset(void *b, int c, size_t len);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like bzero, but does nothing if (s) == NULL.
 void		ft_prot_bzero(void *s, size_t n);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like memcpy, but returns (dst) immediately if either == NULL.
+// 🤔 Maybe useless
 void		*ft_prot_memcpy(void *dst, const void *src, size_t n);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // 💥 TODO can this crash if len is incorrect?
 // Like memmove, but returns (dst) immediately if (src || dst) == NULL.
+// 🤔 Maybe useless
 void		*ft_prot_memmove(void *dst, const void *src, size_t len);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like strlcpy, but returns (prot_len(src)) immediately if (dst || src) == NULL
+// 🤔 ?
 size_t		ft_prot_strlcpy(char *dst, const char *src, size_t dstsize);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like strlcat, but can't crash.
+// 🤔 ?
 size_t		ft_prot_strlcat(char *dst, const char *src, size_t dstsize);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like strchr, but can't crash.
 char		*ft_prot_strchr(const char *s, int c);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like strrchr, but can't crash.
 char		*ft_prot_strrchr(const char *s, int c);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like memchr, but can't crash.
 void		*ft_prot_memchr(const void *s, int c, size_t n);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like strnstr, but can't crash.
 char		*ft_prot_strnstr(const char *haystack,
 				const char *needle, size_t len);
-// 📂 ft++ / prot libc
+
+// 📂 ft++ / 🛡 prot libc
 // Like atoi, but if (str) == NULL, return 0.
 int			ft_prot_atoi(const char *str);
+
 // ❗️ Uses: malloc()
-// 📂 ft++ / prot libc
-// ☢️ Ambiguous return: null input, OR malloc error
+// 📂 ft++ / 🛡 prot libc
 // Like strdup, but returns NULL instead of crashing if (s1) == NULL.
+// ☢️❗️ Ambiguous return: null input, OR malloc error
 char		*ft_prot_strdup(const char *s1);
 
-// TODO write "Protected Available"
-// TODO "Ambiguous Return"
-
 // ❗️ Uses: free()
-// 📂 ft++ / prot bonus
-// ☢️ WARNING: Protection may cause this to silently leak.
+// 📂 ft++ / 🛡 prot bonus
+// ☢️❗️ WARNING: Protection may cause this to silently leak.
 //  Consider using the unprotected version to notice if the function is misused.
 void		ft_prot_lstclear(t_list **lst, void (*del)(void*));
 // ❗️ Uses: free()
-// 📂 ft++ / prot bonus
-// ☢️ WARNING: Protection may cause this to silently leak.
+// 📂 ft++ / 🛡 prot bonus
+// ☢️❗️ WARNING: Protection may cause this to silently leak.
 //  Consider using the unprotected version to notice if the function is misused.
 void		ft_prot_lstdelone(t_list *lst, void (*del)(void*));
 
 ////////////////////////////////////////////////////////////////////////////////
-////  Personal additions ft++  /////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////    /    /    ////
+////  Personal additions ft++  //////////////////////////////  --+----+--  /////
+////////////////////////////////////////////////////////////    /    /    //////
 ////////////////////////////////////////////////////////////////////////////////
+
+//// These two were moved from static
+
+// 📂 ft++ (libc replica)
+int			ft_isspace(char c);
+// 📂 ft++
+// 💥 (dst || src) == NULL
+// Similar to ft_memcpy.
+// Goes from right to left, skips the special case check and does not return.
+void		ft_mini_memcpy_left(void *dst, const void *src, size_t len);
 
 // ❗️ Uses: printf(stdio) FIXME when fixed, uncomment
 // 📂 ft++
